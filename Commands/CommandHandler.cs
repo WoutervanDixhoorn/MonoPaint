b69021b78cf353f03f984aa10f15484c54a6a7f0
WoutervanDixhoorn <9KsmQ3N6>
2021-04-21 15:51:34 +0200

    Squashed commit of the following:
    commit 1668b7f5ca48c54a4c592fe7cd361ee00d337121
    Author: WoutervanDixhoorn <9KsmQ3N6>
    Date:   Wed Apr 21 15:50:48 2021 +0200
    
        -Update-
    
    commit 07b55f85e459d33128a0bfaa42cbe2a1f13dc528
    Author: WoutervanDixhoorn <9KsmQ3N6>
    Date:   Wed Apr 21 15:50:28 2021 +0200
    
        -Forgot Redo-
    
        Added Redo functionality with leftControl + Y

--- Commands/CommandHandler.cs
+++ Commands/CommandHandler.cs
@@ -7,10 +7,13 @@ namespace MonoPaint.Commands
     {
         
         List<ICommand> recentCommands;
+        
+        List<ICommand> undidCommands;
 
         public CommandHandler()
         {
             recentCommands = new List<ICommand>();
+            undidCommands = new List<ICommand>();
         }
 
         public void Execute(ICommand iCommand)
@@ -23,9 +26,21 @@ namespace MonoPaint.Commands
         {
             if(recentCommands.Count > 0){
                 recentCommands[recentCommands.Count-1].Undo();
+                undidCommands.Add(recentCommands[recentCommands.Count-1]);
                 recentCommands.RemoveAt(recentCommands.Count-1);
             }else{
                 Console.WriteLine("Nothing there to undo!");
+            }
+        }
+
+        public void Redo()
+        {
+            if(undidCommands.Count > 0){
+                undidCommands[undidCommands.Count-1].Execute();
+                recentCommands.Add(undidCommands[undidCommands.Count-1]);
+                undidCommands.RemoveAt(undidCommands.Count-1);
+            }else{
+                Console.WriteLine("Nothing there to redo!");
             }
         }
     }
